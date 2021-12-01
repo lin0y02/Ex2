51f9ab31db492275de981ea96f2d5205c4db9751
lin0y02 <linoyroy1@gmail.com>
2021-12-01 23:50:58 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,8 @@
+#ifndef MY_MAT_H_INCLUDED
+#define MY_MAT_H_INCLUDED
+
+void matrix(int mat[sizeOf(mat)][sizeOf(mat)]);
+void ifExist(int i, int j, int mat[sizeOf(mat)][sizeOf(mat)]);
+void shortPath(int i, int j, int mat[sizeOf(mat)][sizeOf(mat)]);
+
+#endif // MY_MAT_H_INCLUDED
